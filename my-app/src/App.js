c2503e3eecc71db5a7fc9fa40f06b3f87eccecd4
Panagiotis R <panagiotis.r@campeongaming.com>
2023-06-13 17:07:23 +0300

    A new change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the first application
+          This is the first application but I made a change
         </p>
       </header>
     </div>
